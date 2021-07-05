ae48f023a584acb9d5ed8e867bf984b03b45bfc6
vanishcode <vanishcode@outlook.com>
2021-07-05 21:04:26 +0800

    feat: 合并 san-template-loader 代码

--- index.js
+++ index.js
@@ -1,34 +1,13 @@
-const { getOptions } = require("loader-utils");
-const aNodeUtils = require("san-anode-utils");
+const {parse} = require('@babel/parser');
+
+const parseTemplateInJS = require('./lib/template-in-js');
+const parseSFC = require('./lib/single-file-component');
 
 module.exports = function (source) {
-    // we don't process entir html file
-    if (source && source.includes('<!DOCTYPE html>')) return source;
-
-    if (this.cacheable) this.cacheable();
-
-    let result = source;
-
-    const options = getOptions(this);
-    const { compileTemplate } = options;
-
-    if (compileTemplate) {
-        const aNode = aNodeUtils.parseTemplate(source);
-
-        switch (compileTemplate) {
-            case "aNode":
-                result = aNode;
-                break;
-            case "aPack":
-                if (aNode.children.length) {
-                    const aPack = aNodeUtils.pack(aNode.children[0]);
-                    result = aPack;
-                }
-                break;
-            default:
-                break;
-        }
+    try {
+        parse(source);
+        return parseTemplateInJS(source);
+    } catch (error) {
+        return parseSFC(source);
     }
-
-    return result;
 };
