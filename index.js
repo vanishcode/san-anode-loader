7a3a572dc345bc55f730087fe9b689722a18927b
vanishcode <vanishcode@outlook.com>
2021-07-12 20:42:47 +0800

    feat: 使用 resourceQuery 处理不同类型文件

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
-const { parse } = require('@babel/parser');
+const path = require('path');
 
 const parseTemplateInJS = require('./lib/template-in-js');
 const parseSFC = require('./lib/single-file-component');
 
 module.exports = function (source) {
-    try {
-        parse(source, {
-            sourceType: 'module',
-            plugins: ['classProperties'],
-        });
-        return parseTemplateInJS.call(this, source);
-    } catch (error) {
-        return parseSFC.call(this, source);
+    if (!this.resourcePath.includes('node_modules')) {
+        const extName = path.extname(this.resourcePath);
+
+        if (
+            extName === '.san' &&
+            this.resourceQuery.includes('type=template')
+        ) {
+            return parseSFC.call(this, source);
+        }
+
+        if (extName === '.js') {
+            return parseTemplateInJS.call(this, source);
+        }
     }
+    return source;
 };
