cea94cc7998e7773cbc20fb90da048ceb7a77d72
vanishcode <vanishcode@outlook.com>
2021-07-13 15:35:15 +0800

    style: normal lint

--- babel.config.js
+++ babel.config.js
@@ -1,17 +1,17 @@
 /**
  * @file babel config
  */
- module.exports = {
+module.exports = {
     presets: [
         [
             '@babel/preset-env',
             {
                 targets: {
-                    node: 'current'
-                }
-            }
-        ]
+                    node: 'current',
+                },
+            },
+        ],
     ],
-    plugins: ["@babel/plugin-proposal-class-properties"],
-    exclude: 'node_modules'
+    plugins: ['@babel/plugin-proposal-class-properties'],
+    exclude: 'node_modules',
 };
